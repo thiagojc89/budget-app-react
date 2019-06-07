139382204e7ff04cbe0575d0f34d8bcebe7075d7
Thiago Julian Cavalcante <thiagojc89@gmail.com>
2019-06-07 18:19:31 -0500

    with a sick nasty move natham help me render the labels in the chart

--- src/ChartComponent/index.js
+++ src/ChartComponent/index.js
@@ -3,12 +3,13 @@ import {Bar, Line, Pie} from 'react-chartjs-2';
 							  
 
 
+
 class ChartComponent extends React.Component {
 	constructor(props){
 		super(props)
 		this.state={
 			chartData:{
-				labels:['RED', 'GREEN','BLUE'],
+				labels: [],
 				datasets:[
 				{
 					label:'COLOR', 
@@ -18,35 +19,76 @@ class ChartComponent extends React.Component {
 						'green',
 						'blue']
 				}]
-			
-
 			},
 		}
 	}
+	getLabels = ()=>{
+		const labels = this.props.allItens.map(item=> item.name)
+		console.log(labels);
+
+		const oldLabels = this.state.chartData.labels 
+		const allLabels = oldLabels.concat(labels)
+
+		console.log("all labels")
+		console.log(allLabels);
+
+		const newChartData = this.state.chartData 
+
+		newChartData.labels = allLabels
+
+		this.setState({
+			chartData: newChartData
+		})
+	}
+	shouldComponentUpdate(nextProps, nextState){
+
+		let flag = false 
+
+		nextProps.allItens.forEach((item, i) => {
+			if (item.name !== this.state.chartData.labels[i]) {
+				flag = true
+			}
+		})
+
+		if (flag) {
+			this.getLabels()
+		}
+
+		return flag
+	}
 	render(){
+		console.log("RENDERING")
+		console.log("chart props:");
+		console.log(this.props.allItens);
+		console.log("chart state:");
+		console.log(this.state);
 		return(
 			    <div className="chartComponent">
 			    	<div className="chart">
 				        <p>chartComponent</p>
-				        <Line
+				        <Bar
 				        	data={this.state.chartData}
-				        	width={100}
-  							height={50}
-				        	options={{legend: {
-                            display: false,
-                            fontColor: 'green'
-                        },
-                        responsive: true,
-                        maintainAspectRatio: false,
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    beginAtZero:true,
-                                    stepSize: 50
-                                }
-                            }]
-                        }}}
-				        	/>
+				     //    	width={100}
+  							// height={50}
+				        	options={{
+				        		legend: {
+	                        	    display: false,
+	                            	fontColor: 'green'
+                        			},
+                        		responsive: true,
+                        		maintainAspectRatio: false,
+                        		scales: {
+                            		yAxes: [{
+                                		ticks: {
+                                    		beginAtZero:true,
+                                    		stepSize: 50
+                                			}
+                            			}]
+                        			}
+                        		}
+                        	}
+
+				        />
 				      </div>
 		        </div>
 		)
