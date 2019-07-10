8d8532291aed2e49e2825de5b31cd9fa5b5abc6d
Thiago Julian Cavalcante <thiagojc89@gmail.com>
2019-07-10 15:39:28 -0500

    add text to the footer

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-const Footer = (props) => {
+const Footer = () => {
     return (
         <div className="footerComponent">
-
-            <div>
-              <br/>
-                <br />
-                <br />
-                <br />
-            </div>
+          <a href="https://www.linkedin.com/in/thiagojc89/">
+            <i class="btn-2 fab fa-linkedin fa-8x"></i>
+          </a>
+          <a href="https://github.com/thiagojc89">
+            <i class="btn-2 fab fa-github fa-8x"></i>
+          </a>
+          <footer id="main-footer">
+            <h5>&copy; Thiago Cavalcante - thiagojc89</h5>
+          </footer>
         </div>
+      
     )
 }
 
